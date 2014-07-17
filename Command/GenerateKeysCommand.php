e8736f6a117b72641d28af500b700493c76b7a84
Roberto Mansfield <robertom@sas.upenn.edu>
2014-07-17 14:46:29 -0400

    Add command to generate encryption keys

---
+++ Command/GenerateKeysCommand.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace SAS\IRAD\FileStorageBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+
+class GenerateKeysCommand extends ContainerAwareCommand {
+
+    protected function configure() {
+
+        $rootDir = $this->getContainer()->get('kernel')->getRootDir();
+        
+        $this
+        ->setName('storage:generate-keys')
+        ->setDescription('Generate encryption keys to encrypt data files at rest in the file system.')
+        ->addOption('path', null, InputOption::VALUE_REQUIRED, "Path for key files", "$rootDir/app/config/keys")
+        ;
+
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output) {
+
+        $key_path = $input->getOption('path');
+        
+        // create our "keys" directory if it doesn't exist
+        if ( !is_dir($key_path) ) {
+            mkdir($key_path, 0770, true);
+        }
+        
+        // path for public and private keys
+        $public_key_path  = "$key_path/public.pem";
+        $private_key_path = "$key_path/private.pem";
+
+        // don't overwrite keys if they already exist
+        if ( file_exists($public_key_path) || file_exists($private_key_path) ) {
+            echo "Key files already exist. Please remove before generating new keys.\n";
+            exit;
+        }
+        
+        // generate keys
+        $output->writeln("Generating new key...");
+        $keys = openssl_pkey_new(array("private_key_bits" => 2048));
+        $output->writeln("Done.");
+        
+        // export private key
+        $private_key = null;
+        openssl_pkey_export($keys, $private_key);
+        
+        // export public key
+        $details = openssl_pkey_get_details($keys);
+        $public_key = $details["key"];
+        
+        $output->writeln("Exporting private key to $private_key_path");
+        file_put_contents($private_key_path, $private_key);
+        
+        $output->writeln("Exporting public key to $public_key_path");
+        file_put_contents($public_key_path, $public_key);
+        
+        chmod($private_key_path, 0660);
+        chmod($public_key_path,  0660);
+        
+        $output->writeln("Done.");
+    }
+
+}
