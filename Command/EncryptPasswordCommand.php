878ba025168ba1a7e82fe352297312f2638ce3ad
Roberto Mansfield <robertom@sas.upenn.edu>
2014-08-08 12:34:21 -0400

    Add additional exception checks to storage:encrypt-pw command

--- Command/EncryptPasswordCommand.php
+++ Command/EncryptPasswordCommand.php
@@ -15,7 +15,6 @@ class EncryptPasswordCommand extends ContainerAwareCommand {
         $this
         ->setName('storage:encrypt-pw')
         ->setDescription('Generate an encrypted password file.')
-        ->addOption('keys', null, InputOption::VALUE_OPTIONAL, "Path for key files")
         ->addOption('path', null, InputOption::VALUE_OPTIONAL, "Path for resulting password file")
         ;
 
@@ -24,53 +23,49 @@ class EncryptPasswordCommand extends ContainerAwareCommand {
     protected function execute(InputInterface $input, OutputInterface $output) {
 
         $root_dir = $this->getContainer()->get('kernel')->getRootDir();
-        $key_path = $input->getOption('keys');
         
-        if ( !$key_path ) {
-            $key_path = "$root_dir/config/keys";
-        }
-
         $pw_file = $input->getOption('path');
         
         if ( !$pw_file ) {
             $pw_file = "$root_dir/config/pw.txt"; 
+        } else {
+            if ( strpos($pw_file, '~') !== false ) {
+                throw new \Exception("Tilde characters in a path are not supported by this script.");
+            }
         }
         
-        // create our "keys" directory if it doesn't exist
+        // create our password directory if it doesn't exist
+        $dir = dirname($pw_file);
         if ( !is_dir(dirname($pw_file)) ) {
-            mkdir($key_path, 0770, true);
+            if ( !mkdir($dir, 0770, true) ) {
+                throw new \Exception("Unable to create directory $dir");
+            }            
         }
-        
-        // path for public and private keys
-        $public_key_path  = "$key_path/public.pem";
-        $private_key_path = "$key_path/private.pem";
 
-        // don't overwrite password file if it already exists
-        if ( !file_exists($public_key_path) || !file_exists($private_key_path) ) {
-            throw new \Exception("Public/private key not found.");
-        }
+        $pw_file = realpath($dir) . '/' . basename($pw_file);
         
         if ( file_exists($pw_file) ) {
             throw new \Exception("Password file \"$pw_file\" already exists.");
         }
         
         $dialog    = $this->getHelperSet()->get('dialog');
-        $password1 = $dialog->askHiddenResponse($output, 'Enter password to encrypt: ');
+        $password1 = null;
+        while ( !$password1 ) {
+            $password1 = $dialog->askHiddenResponse($output, 'Enter password to encrypt: ');
+            if ( !$password1 ) {
+                echo "** Nothing entered **\n";
+            }
+        }
         $password2 = $dialog->askHiddenResponse($output, 'Retype password to confirm: ');
         
         if ( $password1 !== $password2 ) {
             throw new \Exception("Password entries did not match!");
         }
         
-        $encrypted = $this->getContainer()->get('file.encrypted_storage');
+        $encrypted = $this->getContainer()->get('storage.encrypted');
         
-        $keys = array('public_key'  => $public_key_path, 
-                      'private_key' => $private_key_path);
-        
-        $storage = $encrypted->init($pw_file, $keys); 
+        $storage = $encrypted->init($pw_file); 
         $storage->save($password1);
-        
-        chmod($pw_file, 0660);
         
         $output->writeln("Generated file: $pw_file");
     }
